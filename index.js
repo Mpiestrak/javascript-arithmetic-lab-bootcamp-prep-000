61f43570d89ad59b29be4f2d923876b38fa047a6
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-24 19:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ function add(a, b) {
   number = a + b;
   return number;
 }
-
+function subtract(a, b){
+  number = a - b;
+  return number;
+}
+function multiply(a, b){
+  number = a * b;
+  return number
+}
+function divide(a, b){
+  number = a / b;
+  return number
+}
